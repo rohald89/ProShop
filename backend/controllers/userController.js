37d46112c389cb0dbf46757ad9643d97e3b8aa35
Rohald van Merode <rohald_89@live.nl>
2021-08-22 14:27:43 +0200

    created protect middleware to check if the user is authorized for protected routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ import generateToken from '../utils/generateToken.js';
 
 /**
  * @desc    Auth user & get toekn
- * @route   GET /api/users/login
+ * @route   POST /api/users/login
  * @access  Public
  */
 const authUser = asyncHandler(async (req,res) => {
@@ -26,6 +26,27 @@ const authUser = asyncHandler(async (req,res) => {
     }
 });
 
+/**
+ * @desc    Get user profile
+ * @route   GET /api/users/profile
+ * @access  Private
+ */
+ const getUserProfile = asyncHandler(async (req,res) => {
+    const user = await User.findById(req.user._id);
+    if(user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(404)
+        throw new Error('User not found');
+    }
+});
+
 export {
-    authUser
+    authUser,
+    getUserProfile
 };

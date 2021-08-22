97e67c316b79e462544da5bdadcf81485e646232
Rohald van Merode <rohald_89@live.nl>
2021-08-22 20:31:19 +0200

    started working on the profileScreen, added an update form to make changes to the user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -87,10 +87,10 @@ const authUser = asyncHandler(async (req,res) => {
     const user = await User.findById(req.user._id);
 
     if(user) {
-        user.name = req.body.name || user.name
-        user.email = req.body.email || user.email
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
         if(req.body.password) {
-            user.password = req.body.password
+            user.password = req.body.password;
         }
 
         const updatedUser = await user.save();
@@ -104,7 +104,7 @@ const authUser = asyncHandler(async (req,res) => {
         });
 
     } else {
-        res.status(404)
+        res.status(404);
         throw new Error('User not found');
     }
 });

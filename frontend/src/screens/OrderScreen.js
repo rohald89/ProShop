860deb08758210bbd4830f562c35ce5fcaf84999
Rohald van Merode <rohald_89@live.nl>
2021-08-28 20:29:10 +0200

    created util function to round the prices to 2 decimals and finished the OrderScreen

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { getOrderDetails } from '../actions/orderActions';
-
+import { addDecimals } from '../utils/addDecimals';
 
 const OrderScreen = ({ match }) => {
     const orderId = match.params.id;
@@ -15,35 +15,48 @@ const OrderScreen = ({ match }) => {
     const orderDetails = useSelector(state => state.orderDetails);
     const { order, loading, error } = orderDetails;
 
-    const addDecimals = (num) => {
-        return (Math.round(num * 100) / 100).toFixed(2);
+    if (!loading) {    
+        order.itemsPrice = addDecimals(
+          order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+        );
     }
 
-    order.itemsPrice = addDecimals(order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0));
-
     useEffect(() => {
-        dispatch(getOrderDetails(orderId));
-    }, [])
+        if(!order || order._id !== orderId) {
+            dispatch(getOrderDetails(orderId))
+        }
+    }, [order, orderId]);
 
-    return loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : 
-    <>
+    return loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
         <h1>Order {order._id}</h1>
-        <Row>
+            <Row>
                 <Col md={8}>
                     <ListGroup variant='flush'>
                         <ListGroup.Item>
                             <h2>Shipping</h2>
+                            <p><strong>Name: </strong> {order.user.name}</p>
+                            <p><strong>Email: </strong><a href={`mailto: ${order.user.email}`}>{order.user.email}</a></p>
                             <p>
                                 <strong>Address:</strong>
                                 {order.shippingAddress.address}, {order.shippingAddress.city} {' '}
                                 {order.shippingAddress.postalCode}, {order.shippingAddress.country}
                             </p>
+                            {order.isDelivered ? <Message variant='success'>Delivered at {order.deliveredAt}</Message> : <Message variant='danger'>Not Delivered</Message>}
+
                         </ListGroup.Item>
 
                         <ListGroup.Item>
                             <h2>Payment Method</h2>
-                            <strong>Method: </strong>
-                            {order.paymentMethod}
+                            <p>
+                                <strong>Method: </strong>
+                                {order.paymentMethod}
+                            </p>
+                            {order.isPaid ? <Message variant='success'>Paid on {order.paiddAt}</Message> : <Message variant='danger'>Not Paid</Message>}
                         </ListGroup.Item>
 
                         <ListGroup.Item>
@@ -107,7 +120,8 @@ const OrderScreen = ({ match }) => {
                     </Card>
                 </Col>
             </Row>
-    </>
-}
+        </>
+)};
+    
 
 export default OrderScreen
